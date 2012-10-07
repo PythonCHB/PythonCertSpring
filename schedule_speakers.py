4ab378ca229b5704dabdc5cf50b093214edddd17
Jeff Silv erman <jeffsilverm@gmail.com>
2012-10-07 16:07:57 -0700

    Re-did shcedule_speakers.py to add dates, redid Student_list.csv

--- schedule_speakers.py
+++ schedule_speakers.py
@@ -6,20 +6,27 @@
 import sys
 import random
 import string
+import csv
 
-filename = "student_list.txt"
+date_students = {-1:"Oct 16th", 0:"Oct 23rd", 3:"Oct 30th", 6:"Nov 6th", \
+                 9:"Nov 13th", 12:"Nov 20th", 15:"Nov 27th", 18:"Dec 4th",
+                  21:"Dec 11th" }
+filename = "Student_list.csv"
 f = open(filename,"r")
+raw_student_list = csv.reader(f, delimiter='\t', quotechar='"')
 student_list = []
-for l in f:
-    student_list.append( string.strip( l ) )
+for a_line in raw_student_list:
+# Column 0 is the UW netID, column 1 is the UW student number, column 2 is
+# student's name (last name first)
+    student_list.append( string.strip( a_line[2] ) )
 random.shuffle( student_list, )
 counter = 0
-print "----"
 for n in student_list :
+    if counter  in date_students.keys() :
+        print "----", date_students[counter]
     counter += 1
     print counter, n
-    if counter  in [3, 6, 9, 12, 16, 20, 24, 28, 32] :
-        print "----"
+print "----"
 
 
 
