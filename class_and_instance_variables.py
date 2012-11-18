894184d9b2e346cebbfee216e08e1913c7ef64e7
Jeff Silv erman <jeffsilverm@gmail.com>
2012-11-17 23:01:01 -0800

    Get ready for tomorrow

--- class_and_instance_variables.py
+++ class_and_instance_variables.py
@@ -5,14 +5,17 @@
 
 
 class A(object) :
-    c = None          # c is a class variable
+    c = 16          # c is a class variable
     def __init__ ( self,x ) :
         self.i = x
 a1 = A(3)
 a2 = A(4)
-A.c = 16
+print a1.c, a2.c
 print a1 is a2
 print a1.c, a2.c
 print a1.c is a2.c
 print a1.i, a2.i
+a1.c="Vikings"
+print a1.c, a2.c
+print a1.c is a2.c
 
