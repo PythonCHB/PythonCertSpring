35aed8b8d35a2562bb96f0651a3b05bb67337fa7
Jeff Silv erman <jeffsilverm@gmail.com>
2012-11-23 22:56:23 -0800

    Added clarifications to next week's lecture

--- intro_to_polymorphism.py
+++ intro_to_polymorphism.py
@@ -8,24 +8,26 @@ def test_instance_of ( obj, obj_name, obj_class ) :
             "an instance of ", obj_class.__name__
 
 
-test_instance_of ( mi.e, 'e', mi.E )
-test_instance_of ( mi.a, 'a', mi.E )
-test_instance_of ( mi.e, 'e', mi.A )
-test_instance_of ( mi.b, 'b', mi.A )
 
 def dispatcher ( g ):
-    if isinstance(g, mi.A) :
+    if isinstance(g, mi.A) :  
         print g.a
     elif isinstance(g, mi.C) :
         print g.c
     else :
         print g.e
 
+test_instance_of ( mi.e, 'e', mi.E )
+test_instance_of ( mi.a, 'a', mi.E )
+test_instance_of ( mi.e, 'e', mi.A )
+test_instance_of ( mi.b, 'b', mi.A )
+
 dispatcher ( mi.a )
-dispatcher ( mi.b )
+dispatcher ( mi.b ) # b is of type class B which inherits from A
 dispatcher ( mi.c )
-dispatcher ( mi.d )
-dispatcher ( mi.e )
+dispatcher ( mi.d ) # d is of type class D which inherits from C
+dispatcher ( mi.e ) # e is of type class E which inherits from B (and from A)
+                    # and D (and from C), so dispatcher never prints e.e
 try :
     dispatcher ( "gee" )
 except AttributeError, err :
