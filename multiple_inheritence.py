9a56f9c2ab507eeb8f9eaab184ed3a1578a7f996
Jeff Silv erman <jeffsilverm@gmail.com>
2012-11-26 22:35:14 -0800

    Modified multiple_inheritence.py to show left to right ordering of inheritence

--- multiple_inheritence.py
+++ multiple_inheritence.py
@@ -28,13 +28,20 @@ class E(B,D) :
         print "Defined in E"
     e = "ea."   # an abbreviation for "each"
 
-# These 5 statements have to be here because multiple_inheritence.py is imported
+class F(D,B) :
+    def who_ami_i(self) :
+        print "Defined in F"
+    f = "foolish"    # It's a good thing I am in computers, because if I were
+                     # a poet, I would starve
+
+# These 6 statements have to be here because multiple_inheritence.py is imported
 # by intro_to_polymorphism.py
 a = A()
 b = B()
 c = C()
 d = D()
 e = E()
+f = F()
 
 if __name__ == "__main__" :
     print "a.v=",a.v
@@ -42,14 +49,16 @@ if __name__ == "__main__" :
     print "c.v=",c.v
     print "d.v=",d.v
     print "e.v=",e.v
+    print "f.v=",f.v
 
     a.who_am_i()
     b.who_am_i()
     c.who_am_i()
     d.who_am_i()
     e.who_am_i()
+    f.who_am_i()
 
-    print "e.a=",e.a,"e.b=", e.b,"e.c=", e.c,"e.d=", e.d,"e.e=", e.e
+    print "e.a=",e.a,"e.b=", e.b,"e.c=", e.c,"e.d=", e.d,"e.e=", e.e, "f.f=", f.f
     print "a ",("is" if isinstance(a, A) else "is not"), "an instance of A"
     print "a ",("is" if isinstance(a, B) else "is not"), "an instance of B"
     print "b ",("is" if isinstance(b, A) else "is not"), "an instance of A"
@@ -57,4 +66,8 @@ if __name__ == "__main__" :
     print "e ",("is" if isinstance(e, E) else "is not"), "an instance of E"
     print "e ",("is" if isinstance(e, B) else "is not"), "an instance of B"
     print "e ",("is" if isinstance(e, C) else "is not"), "an instance of C"
-    
+    print "f ",("is" if isinstance(f, F) else "is not"), "an instance of F"
+    print "f ",("is" if isinstance(f, B) else "is not"), "an instance of B"
+    print "f ",("is" if isinstance(f, C) else "is not"), "an instance of C"
+    print "f ",("is" if isinstance(f, E) else "is not"), "an instance of E"
+
