1a1bb2cf3897bae7efd7c202d94d12542b462fe5
Jeff Silverman <jeffsilverm@gmail.com>
2013-01-15 20:35:43 -0800

    Fixed the assert statements

--- snafu_ORIGINAL.py
+++ snafu_ORIGINAL.py
@@ -6,10 +6,11 @@
 #
 #
 import math
-import pdb; pdb.set_trace()   # for ipython
+import pdb
+pdb.set_trace()   # for ipython, cpython doesn't need it
 
 def discriminant(A, B, C) :
-    assert A != 0.0
+    assert A != 0.0, "The quadratic coefficient is 0.0"
 # Really should be 4 instead of 2, in case you forgot    
     r = math.sqrt(B*B - 2*A*C)
     return r
@@ -19,10 +20,12 @@ def abs_err(a, b):
 
 def test_function (A, B, C, x, yt, err_limit ) :
     y = A*x*x+B*x+C
-    assert abs_err(y, yt) <= err_limit
+    assert abs_err(y, yt) <= err_limit,\
+       "The test function returns an incorrect value %f should be %f" % (y, yt)
     return y
 
 #  This is a useless comment
+print "Good values to try include 32,16,0, 0,32,16, and 0,16,32"
 A = float(raw_input("Enter A "))
 B = float(raw_input("Enter B "))
 C = float(raw_input("Enter C "))
@@ -30,8 +33,10 @@ C = float(raw_input("Enter C "))
 try :
     d = discriminant(C, B, A)
 except AssertionError, e:
+    print "AssertionError raised, reason is %s" % e
     pdb.set_trace()
 except ValueError, e:
+    print "ValueError raised, reason is %s" % e
     pdb.set_trace()
     
 
