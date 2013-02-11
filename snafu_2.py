38426d930562aff47e201a4c242578b0bc829d17
Jeff Silv erman <jeffsilverm@gmail.com>
2013-02-10 21:26:44 -0800

    various stages in the process of fixing snafu.py

--- snafu_2.py
+++ snafu_2.py
@@ -11,7 +11,7 @@ pdb.set_trace()   # for ipython, cpython doesn't need it
 
 def discriminant(A, B, C) :
     """Return the discriminant of the quadratic: B^2-4AC"""
-    r = math.sqrt(B*B - 2*A*C)
+    r = math.sqrt(B*B - 4*A*C)
     return r
 
 def abs_err(a, b):
@@ -21,15 +21,15 @@ def test_function (A, B, C, x, yt, err_limit ) :
     y = A*x*x+B*x+C
     assert abs_err(y, yt) <= err_limit,\
        "The test function returns an incorrect value %f should be %f" % (y, yt)
-    return
+    return 
 
 #  This is a useless comment
-print "Good values to try include 32,16,0, 32,16,4 and 0,16,32"
+print "Good values to try include 32,16,0, 1,4,3 and 0,16,32"
 A = float(raw_input("Enter A "))
 B = float(raw_input("Enter B "))
 C = float(raw_input("Enter C "))
 
-d = discriminant(C, B, A)
+d = discriminant(A, B, C)
     
 
 r1 = (-B + d)/(2*A)
