7f2291bd62b4c1f4dffc57487e24c695345ba425
Chris Barker <Chris.Barker@noaa.gov>
2013-04-08 22:08:40 -0700

    re-arranged propertied example code

--- week-03/code/decorators/p_wrapper_solution.py
+++ week-03/code/decorators/p_wrapper_solution.py
@@ -15,11 +15,34 @@ func()
 
 # the simple decorator
 
-def p_wrapper(f):
+def p_wrapper(func):
     def function(*args, **kwargs):
-        result = f(*args, **kwargs)
+        result = func(*args, **kwargs)
         return "<p>" + result + "</p>"
     return function
+
+
+# fancy one using a class:
+# this lets you make a decorator with some custom input
+# the argument to the __init__ sets what tag you want
+# this creates a custom decorator
+# the __call__ method is the decorator itself.
+class tag_wrapper(object):
+    def __init__(self, tag='p' ):
+        """
+        inititilze the decorator class with the tag you want
+        """
+        self.open_tag = "<%s>"%tag
+        self.close_tag = "</%s>"%tag
+
+    def __call__(self, func, *args, **kwargs):
+        """
+        The actual decorator function.
+
+        using lambda - 'cause why not?
+        """
+        return lambda *args, **kwargs: self.open_tag + func(*args, **kwargs) + self.close_tag
+
 
 # give it a try:
 if __name__ == "__main__":
@@ -61,4 +84,19 @@ if __name__ == "__main__":
     print func2(3)
     print func2(3, 5)
     print func2(3, 5, 7)
+
+    ## and try the class version:
+
+    @tag_wrapper('h1')
+    def func2(x, y=4, z=2):
+        return "the sum of %s and %s and %s is %s"%(x, y, z, x+y+z)
+
+    print func2(3,4)
+
+    @tag_wrapper('div')
+    def func2(x, y=4, z=2):
+        return "the sum of %s and %s and %s is %s"%(x, y, z, x+y+z)
+
+    print func2(5,6,7)
+
 
