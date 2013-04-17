f3da7f77d32c02dc28e7af299ebbbcc41adddc71
Jeff Silv erman <jeffsilverm@gmail.com>
2013-04-17 15:53:23 -0700

    Two redis demonstrations.  redis_demo_bs_spider.py is a web spider.  test_redis_persistence.py tests persistence in redis

---
+++ week-05/redis_demo_bs_spider.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+"""This software traverses the web and loads the data into a redis database
+The key to the database is a URL, the values of the keys are a list of
+outbound urls.  If the url has already been visited, then it is skipped"""
+
+from bs4 import BeautifulSoup
+import redis
+import time
+import urllib2
+import socket   # because we might handle a socket timeout exception
+import pydot    # python library to generate graphs
+import sys
+
+if len(sys.argv) > 1 :
+    max_num_of_links = int(sys.argv[1])
+else:
+    max_num_of_links = 100   # reasonable default value
+
+VISITED_LIST = "redis_demo_bs_spider_visited_list"  # keyname of the visited list
+TO_VISIT_SET = "redis_demo_bs_spider_to_visit_set"  # keyname of the set of pages
+                                                    # to visit
+# Open a connection to the database
+# r_server = redis.Redis("108.59.89.216", port=6379, db=0)
+r_server = redis.Redis("localhost", port=6379, db=0)
+
+
+# Clear out the keys in the database that hold the list of sites that we have visited
+# and the set of sites to visit.
+r_server.delete(VISITED_LIST )
+r_server.delete(TO_VISIT_SET )
+# Insert all the specified values at the tail of the list stored at key. If key
+# does not exist, it is created as empty list before performing the push
+# operation.  Start at commercialventvac.com, which is well connected.
+r_server.sadd(TO_VISIT_SET, "http://www.commercialventvac.com")
+start_time = time.time()
+for link_cnt in range(0,max_num_of_links):
+# Removes and returns a random element from the set value stored at key.
+    this_page = r_server.spop(TO_VISIT_SET)
+    print "**** Crawling %s visited %d " % ( this_page, r_server.llen( VISITED_LIST ))
+    try:
+        response = urllib2.urlopen(this_page, timeout=15)    # timeout is in seconds
+    except (urllib2.URLError, socket.timeout, urllib2.HTTPError, ValueError),e:
+        print "The URL %s failed due to %s - skipping" % ( this_page, e)
+        continue
+    
+    html_doc = response.read()
+# Push this_page to the end of the visited list    
+    r_server.rpush(VISITED_LIST, this_page)
+    soup = BeautifulSoup(html_doc, "html5lib")
+    link_list = soup.find_all('a')
+    for link in link_list:
+        if 'href' in link.attrs :
+            url = link.attrs['href']
+        
+    # This is necessary because if this key already exists and it is not a set
+    # type, then the sadd command throws a
+    # "Operation against a key holding the wrong kind of value"
+    # This is a leftover from previous runs of this software, which used a different
+    # data structure
+            if r_server.exists(url) :
+    # Returns the string representation of the type of the value stored at key. The
+    # different types that can be returned are: string, list, set, zset and hash.            
+                if r_server.type(url) != "set" :
+                    r_server.delete(url)
+    # Add the url to the set of urls that we need to visit.  If the URL is already
+    # in the set, then this doesn't do anything.
+            r_server.sadd(TO_VISIT_SET, url)
+            r_server.sadd(this_page, url)
+            
+end_time = time.time()
+g = pydot.Dot(graph_type='digraph')        # digraph => directed graph
+while r_server.llen(VISITED_LIST) > 0:
+# Get the next URL in the list of URLs that we visited  
+    url = r_server.lpop(VISITED_LIST)
+    source_node = pydot.Node(url)
+    g.add_node( source_node )
+# is the set of links that URL points to empty?
+    while r_server.scard(url) > 0 :
+        link = r_server.spop(url)
+        print "%s => %s"% ( url, link )
+        destination_node = pydot.Node(link)
+        g.add_node(destination_node)
+        g.add_edge(pydot.Edge(source_node, destination_node))
+r_server.bgsave()       # Tell the Redis server to save its data to disk. Unlike
+                        # save(), this method is asynchronous and returns
+                        # immediately.
+g.write_png('example2_graph.png')
+g.write_dot('example2_graph.dot')
+execution_time = end_time - start_time
+print "Execution time was %f to collect %d links or %f links/second" % (execution_time,
+                max_num_of_links, max_num_of_links/execution_time)
---
+++ week-05/test_redis_persistence.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+#
+# This script tests the persistence of a redis database
+import redis
+import socket   # because we might handle a socket timeout exception
+import os
+import sys
+import subprocess
+import random
+import time
+
+def check_super_user():
+    """This function checks that the process has super user (root) privileges.
+This software will only run on UNIX.  It will not run under Cygwin.  I don't
+know if this software will run under MS-Windows"""
+
+    egid = os.getegid()
+    euid = os.geteuid()
+
+    return egid == 0 and euid == 0
+  
+def stop_start_redis(op):
+    """This function either starts or stops the redis-server daemon, depending
+on the value of op.  If op is start, then the daemon is started.  If op is stop
+then the daemon is stopped.  If other values are used, then the results are
+unpredictable"""
+    ret_status = subprocess.call('service redis-server %s' % op, shell=True)
+    if ret_status != 0 :
+        print "Failed to %s the redis-server" % op
+        sys.exit(1)
+
+
+def test_persistence ( r_server, save=True ):
+    # Generate a random string, and see if it is preserved across database calls    
+    value = str(random.random())
+
+    r_server.set('foo', value)
+    if save :
+        r_server.save()         # synchronous save - will block until complete
+        print "Shutting down the server after saving state"
+    else :
+        print "Shutting down the server without saving state first"
+    stop_start_redis("stop")
+    stop_start_redis("start")
+
+    while True:
+        print "Waiting for the daemon to start"
+        time.sleep(5.0)
+        try:
+            results = r_server.get('foo')
+        except redis.exceptions.ConnectionError:
+            print "The daemon isn't accepting connections yet - wait"
+        else:
+            break
+    assert results == value, """The value was *not* preserved across daemon
+restarts.  save is %s""" % str(save)
+    print "The value was preserved across daemon restart. save is %s" % str(save)
+    
+
+if __name__ == "__main__" :
+    if not check_super_user() :
+        print """This program must run with root privileges because it stops and
+the restarts the redis server"""
+        sys.exit(1)
+    # Open a connection to the database
+    # r_server = redis.Redis("108.59.89.216", port=6379, db=0)
+    r_server = redis.Redis("localhost", port=6379, db=1)
+
+    test_persistence ( r_server, save=True )
+    try :
+        test_persistence ( r_server, save=False )
+
+    except AssertionError:
+        print "The value was not persisted when the state of the database was not saved"
+
