8138c8139fa5ce61613a331162a674cf46a7a6ac
Jeff Silverman <jeffsilverm@gmail.com>
2013-05-19 21:23:53 -0700

    Added file dialog boxes

--- week-09/code/basic_app_4.py
+++ week-09/code/basic_app_4.py
@@ -7,6 +7,19 @@ This version adds a basic menu bar with a file menu
 """
 
 import wx
+import os
+
+#---------------------------------------------------------------------------
+
+# This is how you pre-establish a file filter so that the dialog
+# only shows the extension(s) you want it to.
+wildcard = "Python source (*.py)|*.py|"     \
+           "Compiled Python (*.pyc)|*.pyc|" \
+           "SPAM files (*.spam)|*.spam|"    \
+           "Egg file (*.egg)|*.egg|"        \
+           "All files (*.*)|*.*"
+
+#---------------------------------------------------------------------------
 
 
 class AppLogic(object):
@@ -19,6 +32,7 @@ class AppLogic(object):
     In a real app, this would be a substantial collection of 
     modules, classes, etc...
     """
+
     def file_open(self, filename="default_name"):
         """This method opens a file"""
         print "Open a file: "
@@ -29,6 +43,9 @@ class AppLogic(object):
         print "Close a file: "
         print "I'd be closing a file now"
  
+
+
+
 
 class TestFrame(wx.Frame):
     def __init__(self, app_logic, *args, **kwargs):
@@ -64,18 +81,102 @@ class TestFrame(wx.Frame):
     def onButton(self, evt=None):
         print "You pushed the button!"
 
-    def onOpen(self, evt=None):
-        print "open menu selected"
-        self.app_logic.file_open()
-
-    def onClose(self, evt=None):
-        print "close menu selected"
-        self.app_logic.file_close()
 
     def onExit(self, evt=None):
         print "Exit the program here"
         print "The event passed to onExit is type ", type(evt),
         self.Close()
+
+    def onClose(self, evt=None):
+        print "close menu selected"
+        self.app_logic.file_close()
+
+
+    def onNew ( self, evt=None ):
+        """This method creates a new file"""
+
+        # Create the dialog. In this case the current directory is forced as the starting
+        # directory for the dialog, and no default file name is forced. This can easilly
+        # be changed in your program. This is an 'save' dialog.
+        #
+        # Unlike the 'open dialog' example found elsewhere, this example does NOT
+        # force the current working directory to change if the user chooses a different
+        # directory than the one initially set.
+        dlg = wx.FileDialog(
+            self, message="Save file as ...", defaultDir=os.getcwd(), 
+            defaultFile="", wildcard=wildcard, style=wx.SAVE )
+
+        # This sets the default filter that the user will initially see. Otherwise,
+        # the first filter in the list will be used by default.
+        dlg.SetFilterIndex(2)
+
+        # Show the dialog and retrieve the user response. If it is the OK response, 
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            path = dlg.GetPath()
+            print "In onNew, the path is %s" % path
+            # Normally, at this point you would save your data using the file and path
+            # data that the user provided to you, but since we didn't actually start
+            # with any data to work with, that would be difficult.
+            # 
+            # The code to do so would be similar to this, assuming 'data' contains
+            # the data you want to save:
+            #
+            # fp = file(path, 'w') # Create file anew
+            # fp.write(data)
+            # fp.close()
+            #
+            # You might want to add some error checking :-)
+            #
+        else :
+            print "The file dialog was canceled before anything was selected"
+
+        # Note that the current working dir didn't change. This is good since
+        # that's the way we set it up.
+
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+
+        
+
+    
+    def onOpen(self, evt=None):
+        """This method opens an existing file"""
+        print "Open a file: "
+        # Create the dialog. In this case the current directory is forced as the starting
+        # directory for the dialog, and no default file name is forced. This can easilly
+        # be changed in your program. This is an 'open' dialog, and allows multitple
+        # file selections as well.
+        #
+        # Finally, if the directory is changed in the process of getting files, this
+        # dialog is set up to change the current working directory to the path chosen.
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=os.getcwd(), 
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.OPEN | wx.CHANGE_DIR
+            )
+
+        # Show the dialog and retrieve the user response. If it is the OK response, 
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            # This returns a Python list of files that were selected.
+            path = dlg.GetPath()
+            print "I'd be opening file in onOpen ", path
+            self.app_logic.file_open( path )
+        else :
+            print "The file dialog was canceled before anything was selected"
+            
+
+
+
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+
+
 
 
 class TestApp(wx.App):
